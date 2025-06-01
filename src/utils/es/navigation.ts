7d996fea1d75a51d56b290037fb6d4b02942c243
Enmanoell Mosley <enmamosley@gmail.com>
2025-06-01 16:39:03 -0600

    Borrando cosas innecesarias pt1

--- src/utils/es/navigation.ts
+++ src/utils/es/navigation.ts
@@ -1,28 +1,20 @@
 
 const navBarLinks = [
   { name: "Accueil", url: "/es" },
-  { name: "Produits", url: "/es/products" },
   { name: "Services", url: "/es/services" },
-  { name: "Blog", url: "/es/blog" },
+
   { name: "Contact", url: "/es/contact" },
 ];
 
 const footerLinks = [
   {
-    section: "Écosystème",
+    section: "",
     links: [
-      { name: "Documentation", url: "/es/welcome-to-docs/" },
-      { name: "Outils et Équipements", url: "/es/products" },
-      { name: "Services de Construction", url: "/es/services" },
     ],
   },
   {
-    section: "Société",
+    section: "",
     links: [
-      { name: "À propos de nous", url: "#" },
-      { name: "Blog", url: "/es/blog" },
-      { name: "Carrières", url: "#" },
-      { name: "Clients", url: "#" },
     ],
   },
 ];
--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,28 +1,22 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
   { name: "Services", url: "/services" },
-  { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ecosystem",
+    section: "",
     links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
+      { name: "", url: "/services" },
     ],
   },
   {
-    section: "Company",
+    section: "",
     links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
+      { name: "", url: "/contact" },
+
     ],
   },
 ];
